1859b5fa7ed363f09e9a067366c64716dd7bfb51
maxime-boussin <maxime.boussin35@gmail.com>
2024-12-12 11:25:49 +0100

    Add external data + final focus

---
+++ data.js
@@ -0,0 +1,131 @@
+
+const data = {
+    "matchs": {
+        "winner": [
+            [
+                { "id": "1-1",  "status": 1, "bgaId": "589684177"},
+                { "id": "1-2",  "status": 1, "bgaId": "589637996"}, 
+                { "id": "1-3",  "status": 2, "bgaId": "588823626"}, 
+                { "id": "1-4",  "status": 2, "bgaId": "590168870"}, 
+                { "id": "1-5",  "status": 2, "bgaId": "589626262"}, 
+                { "id": "1-6",  "status": 2, "bgaId": "589778280"}, 
+                { "id": "1-7",  "status": 1, "bgaId": "589654761"}, 
+                { "id": "1-8",  "status": 2, "bgaId": ""}, 
+                { "id": "1-9",  "status": 2, "bgaId": "589692122"}, 
+                { "id": "1-10", "status": 1, "bgaId": "589230327"}, 
+                { "id": "1-11", "status": 1, "bgaId": "589813972"}, 
+                { "id": "1-12", "status": 2, "bgaId": "589446459"}, 
+                { "id": "1-13", "status": 1, "bgaId": "589821707"}, 
+                { "id": "1-14", "status": 2, "bgaId": "589711880"}, 
+                { "id": "1-15", "status": 2, "bgaId": ""}, 
+                { "id": "1-16", "status": 1, "bgaId": "589314654"}
+            ],
+            [
+                { "id": "2-1", "status": 1, "bgaId": "591162586"},
+                { "id": "2-2", "status": 1, "bgaId": "590488632"},
+                { "id": "2-3", "status": 1, "bgaId": ""},
+                { "id": "2-4", "status": 1, "bgaId": "591099253"},
+                { "id": "2-5", "status": 2, "bgaId": "590977190"},
+                { "id": "2-6", "status": 1, "bgaId": "590692496"},
+                { "id": "2-7", "status": 2, "bgaId": "590192075"}, 
+                { "id": "2-8", "status": 2, "bgaId": "590095090"}
+            ],
+            [ 
+                { "id": "3-1", "status": 2, "bgaId": "591828083"},
+                { "id": "3-2", "status": 1, "bgaId": "591943645"},
+                { "id": "3-3", "status": 2, "bgaId": "593347067"},
+                { "id": "3-4", "status": 2, "bgaId": "592448127"}
+            ],
+            [
+                { "id": "4-1", "status": 1, "bgaId": "592395398"},
+                { "id": "4-2", "status": 2, "bgaId": ""}
+            ],
+            [
+                { "id": "5-1", "status": 2, "bgaId": "597156974"}
+            ]
+        ],
+        "loser": [
+            [
+                { "id": "1-1", "status": 2, "bgaId": "590099485"},   
+                { "id": "1-2", "status": 2, "bgaId": "590609687"}, 
+                { "id": "1-3", "status": 2, "bgaId": "590262389"}, 
+                { "id": "1-4", "status": 2, "bgaId": "590262389"},  
+                { "id": "1-5", "status": 1, "bgaId": "590484255"},
+                { "id": "1-6", "status": 2, "bgaId": "590117597"},  
+                { "id": "1-7", "status": 1, "bgaId": "590264692"}, 
+                { "id": "1-8", "status": 2, "bgaId": "590190210"}
+            ],
+            [
+                { "id": "2-1", "status": 1, "bgaId": ""},   
+                { "id": "2-2", "status": 1, "bgaId": "590638876"}, 
+                { "id": "2-3", "status": 2, "bgaId": "591981731"}, 
+                { "id": "2-4", "status": 2, "bgaId": ""},  
+                { "id": "2-5", "status": 2, "bgaId": "592009065"},
+                { "id": "2-6", "status": 2, "bgaId": "590761450"},  
+                { "id": "2-7", "status": 2, "bgaId": "592029925"}, 
+                { "id": "2-8", "status": 2, "bgaId": "592651949"}
+            ],
+            [ 
+               { "id": "3-1", "status": 2, "bgaId": "592248290"},
+               { "id": "3-2", "status": 1, "bgaId": ""}, 
+               { "id": "3-3", "status": 2, "bgaId": "592741336"}, 
+               { "id": "3-4", "status": 1, "bgaId": "593120637"}
+            ],
+            [ 
+                { "id": "4-1", "status": 2, "bgaId": "593776912"},   
+                { "id": "4-2", "status": 1, "bgaId": "594173517"}, 
+                { "id": "4-3", "status": 1, "bgaId": "593838449"}, 
+                { "id": "4-4", "status": 2, "bgaId": "593746136"}
+            ],
+            [
+                { "id": "5-1", "status": 2, "bgaId": "594604196"},
+                { "id": "5-2", "status": 2, "bgaId": "594727610"}
+            ],
+            [
+                { "id": "6-1", "status": 2, "bgaId": "596812788"},   
+                { "id": "6-2", "status": 2, "bgaId": "596116094"}
+            ],
+            [
+                { "id": "7-1", "status": 1, "bgaId": "600149827"}
+            ],
+            [
+                { "id": "8-1", "status": 1, "bgaId": "600609585"}
+            ]
+        ],
+        "finale": { "id": "1-1", "status": 0, "score": "0 - 0", "bgaId": ""}
+    },
+    "players": [
+        { "id": 1,  "name": "McFlaïe",        "hero": "Teija",    "bg": "m", "deck": "01JC5MVD50S3QNYABHXBRSM7D3" },
+        { "id": 2,  "name": "Bliisters",      "hero": "Rin",      "bg": "m", "deck": "01JB9ZD4VD1N7XHKB6BV1DGG6V" },
+        { "id": 3,  "name": "Hurricane",      "hero": "Afanas",   "bg": "y", "deck": "01JCH57RPH0P637ZFMF48ZXQG0" },
+        { "id": 4,  "name": "Malo",           "hero": "Fen",      "bg": "l", "deck": "01JCDZGDVPPNMA7SSKGDMV1JCH" },
+        { "id": 5,  "name": "Loar",           "hero": "Rin",      "bg": "m", "deck": "01JCKYKYV121BWA8N8B69N51VK" },
+        { "id": 6,  "name": "JoliRougeGorge", "hero": "Lindiwe",  "bg": "y", "deck": "01J9VHCK8DR408E0SC1Y16Q15M" },
+        { "id": 7,  "name": "Autilis",        "hero": "Kojo",     "bg": "b", "deck": "01JCMQDRAW323KG0VRMS8EHVKZ" },
+        { "id": 8,  "name": "Zupaw",          "hero": "Teija",    "bg": "m", "deck": "01JAQE4CJ3KCVAEZJVN2YHECBK" },
+        { "id": 9,  "name": "Loutre",         "hero": "Teija",    "bg": "m", "deck": "01J9K7WGNWT178EE34ZMX33VV7" },
+        { "id": 10, "name": "GrankorSalade",  "hero": "Afanas",   "bg": "y", "deck": "01JB7WEDP3H5SKMPP4RQD7CQQN" },
+        { "id": 11, "name": "Nijisa",         "hero": "Subhash",  "bg": "a", "deck": "01JBQEKED96H4W66SNGEJJ1E59" },
+        { "id": 12, "name": "ZazaLaRiboule",  "hero": "Fen",      "bg": "l", "deck": "01J7KHVJBRV23VVDKXP57PA25R" },
+        { "id": 13, "name": "Yann",           "hero": "Teija",    "bg": "m", "deck": "01J9TEA0HYS5HEKA9WSH8XHD7K" },
+        { "id": 14, "name": "Raloufifa",      "hero": "Afanas",   "bg": "y", "deck": "01JCFTKG84EP8VGHPDJB0GNDPX" },
+        { "id": 15, "name": "EdgeSouls",      "hero": "Sigismar", "bg": "o", "deck": "01J89YQQPFM6QXEZFJ1XH2WH96" },
+        { "id": 16, "name": "Irex",           "hero": "Waru",     "bg": "o", "deck": "01JCZC6GVMXQBAJR5HH49J6ZYM" },
+        { "id": 17, "name": "Djaack",         "hero": "Basira",   "bg": "b", "deck": "01JCTJ7QYJX9314VXP4VPFFRS8" },
+        { "id": 18, "name": "EverydayLife",   "hero": "Waru",     "bg": "o", "deck": "01JCENTNGZQVS9KZKCYSQJ66KN" },
+        { "id": 19, "name": "RoyaumeDuDé",    "hero": "Auraq",    "bg": "l", "deck": "01JBZ5E0KMWRSBDQDZM26BEX6G" },
+        { "id": 20, "name": "Pikevchu",       "hero": "Rin",      "bg": "m", "deck": "01JBXSS6YQT3YQ8JSFH1QREJC4" },
+        { "id": 21, "name": "Nimrel",         "hero": "Fen",      "bg": "l", "deck": "01JCKTG55YYD0GVMSZDCD8N890" },
+        { "id": 22, "name": "Folzoqua",       "hero": "Teija",    "bg": "m", "deck": "01J837K9RE6HD61H3AVVBHR4BM" },
+        { "id": 23, "name": "Calcifer",       "hero": "Akesha",   "bg": "y", "deck": "01JCRZEXPJB7KMV96DN86YMF8F" },
+        { "id": 24, "name": "Marbull",        "hero": "Sigismar", "bg": "o", "deck": "01JCP8N34RWH53259RCBR47992" },
+        { "id": 25, "name": "Y04nn",          "hero": "Afanas",   "bg": "y", "deck": "01J9NZFC7SH9N87RDJCAFGB1DF" },
+        { "id": 26, "name": "Xathros",        "hero": "Kojo",     "bg": "b", "deck": "01JCRWZTSFK7NJZ4QA16DZSRMQ" },
+        { "id": 27, "name": "Hedi",           "hero": "Afanas",   "bg": "y", "deck": "01JCTFWNCK13EMXN756873AFYH" },
+        { "id": 28, "name": "Keg",            "hero": "Fen",      "bg": "l", "deck": "01JCK3EHSJZPN571FDCW9H1PFD" },
+        { "id": 29, "name": "Edco",           "hero": "Auraq",    "bg": "l", "deck": "01JAK3MRP1NDFW8AFVD2FC16P8" },
+        { "id": 30, "name": "Gothon",         "hero": "Fen",      "bg": "l", "deck": "01J9JYV79MYVQZFZRK94FJ7M0F" },
+        { "id": 31, "name": "Xanders",        "hero": "Treyst",   "bg": "a", "deck": "01J9HVZV48TXKDQDQTJ7H3VVM1" },
+        { "id": 32, "name": "JacquesCeler",   "hero": "Treyst",   "bg": "a", "deck": "01JAB5ZDHB8DQ2N6RA6NGKFW30" }
+    ]
+};
