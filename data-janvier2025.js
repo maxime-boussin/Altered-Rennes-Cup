4f25e6531ce64189311ccc91077521293e4b0835
maxime-boussin <maxime.boussin35@gmail.com>
2025-01-12 17:35:42 +0100

    Saison 2

---
+++ data-janvier2025.js
@@ -0,0 +1,48 @@
+
+const data = {
+    "matchs": {
+        "winner": [
+            [
+                { "id": "1-1",  "status": 0, "bgaId": ""},
+                { "id": "1-2",  "status": 0, "bgaId": ""}, 
+                { "id": "1-3",  "status": 0, "bgaId": ""}, 
+                { "id": "1-4",  "status": 0, "bgaId": ""}, 
+                { "id": "1-5",  "status": 0, "bgaId": ""}, 
+                { "id": "1-6",  "status": 0, "bgaId": ""}, 
+                { "id": "1-7",  "status": 0, "bgaId": ""}, 
+                { "id": "1-8",  "status": 0, "bgaId": ""}
+            ],
+            [
+                { "id": "2-1", "status": 0, "bgaId": ""},
+                { "id": "2-2", "status": 0, "bgaId": ""},
+                { "id": "2-3", "status": 0, "bgaId": ""},
+                { "id": "2-4", "status": 0, "bgaId": ""}
+            ],
+            [ 
+                { "id": "3-1", "status": 0, "bgaId": ""},
+                { "id": "3-2", "status": 0, "bgaId": ""}
+            ]
+        ],
+        "loser": [
+        ],
+        "finale": { "id": "1-1", "status": 0, "score": "", "bgaId": ""}
+    },
+    "players": [
+        { "id": 1,  "name": "Hedi",                      "hero": "Afanas",   "bg": "y", "deck": "01JCTFWNCK13EMXN756873AFYH" },
+        { "id": 2,  "name": "2nd - Poule 2",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 3,  "name": "1er - Poule 3",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 4,  "name": "2nd - Poule 4",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 5,  "name": "1er - Poule 5",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 6,  "name": "2nd - Poule 6",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 7,  "name": "1er - Poule 7",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 8,  "name": "2nd - Poule 8",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 9,  "name": "1er - Poule 8",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 10, "name": "2nd - Poule 7",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 11, "name": "1er - Poule 6",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 12, "name": "2nd - Poule 5",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 13, "name": "1er - Poule 4",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 14, "name": "2nd - Poule 3",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 15, "name": "Xanders",                   "hero": "Waru",     "bg": "o", "deck": "01JGP7BXG78VQGFN0GQXRNHVEP" },
+        { "id": 16, "name": "2nd - Poule 1",             "hero": "",         "bg": "z", "deck": "" },
+    ]
+};
