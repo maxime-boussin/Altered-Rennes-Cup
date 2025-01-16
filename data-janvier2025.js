2d9e96be640f0691d1575d3238877130d18736a0
maxime-boussin <maxime.boussin35@gmail.com>
2025-01-16 13:07:31 +0100

    Update data-janvier2025.js

--- data-janvier2025.js
+++ data-janvier2025.js
@@ -30,7 +30,7 @@ const data = {
     "players": [
         { "id": 1,  "name": "Hedi",                      "hero": "Afanas",   "bg": "y", "deck": "01JCTFWNCK13EMXN756873AFYH" },
         { "id": 2,  "name": "Ashaf",                     "hero": "Lindiwe",  "bg": "y", "deck": "01JAGNC68151KYFTN3ZZCTMSF5" },
-        { "id": 3,  "name": "1er - Poule 3",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 3,  "name": "Djaack",                    "hero": "Basira",   "bg": "b", "deck": "01JHJBQX9P6TCABCAJ05AVEKAW" },
         { "id": 4,  "name": "Renable",                   "hero": "Treyst",   "bg": "a", "deck": "01JBYFSWHZ535Y38VQYTPQY971" },
         { "id": 5,  "name": "McFlaie",                   "hero": "Teija",    "bg": "m", "deck": "01JDQGREFQQN7FHQ17D99WEFV4" },
         { "id": 6,  "name": "Marbull",                   "hero": "Fen",      "bg": "l", "deck": "01JGAJP45YT67ASJWHGN3TDWJB" },
@@ -40,9 +40,9 @@ const data = {
         { "id": 10, "name": "Y04nn",                     "hero": "Afanas",   "bg": "y", "deck": "01J9NZFC7SH9N87RDJCAFGB1DF" },
         { "id": 11, "name": "XAT",                       "hero": "Kojo",     "bg": "b", "deck": "01JF5TNWSX3RWTMVQHQATP4N87" },
         { "id": 12, "name": "2nd - Poule 5",             "hero": "",         "bg": "z", "deck": "" },
-        { "id": 13, "name": "JacquesCeler",             "hero": "Treyst",    "bg": "a", "deck": "01JAB5ZDHB8DQ2N6RA6NGKFW30" },
-        { "id": 14, "name": "2nd - Poule 3",             "hero": "",         "bg": "z", "deck": "" },
+        { "id": 13, "name": "JacquesCeler",              "hero": "Treyst",   "bg": "a", "deck": "01JAB5ZDHB8DQ2N6RA6NGKFW30" },
+        { "id": 14, "name": "JoliRougeGorge",            "hero": "Afanas",   "bg": "y", "deck": "01JFASZZ991E060RG35Y9WSJ6J" },
         { "id": 15, "name": "Xanders",                   "hero": "Waru",     "bg": "o", "deck": "01JGP7BXG78VQGFN0GQXRNHVEP" },
-        { "id": 16, "name": "Nijisa",                 "hero": "Treyst",   "bg": "a", "deck": "01JFW6ESFGX83B945HHD017WB8" },
+        { "id": 16, "name": "Nijisa",                    "hero": "Treyst",   "bg": "a", "deck": "01JFW6ESFGX83B945HHD017WB8" },
     ]
 };
